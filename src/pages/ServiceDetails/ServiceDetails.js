2ccfb49ddf5f3d4a3336bd418cd97afda97a29bc
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-10-19 12:43:14 +0600

    all file update

--- src/pages/ServiceDetails/ServiceDetails.js
+++ src/pages/ServiceDetails/ServiceDetails.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import detail from '../../images/detail.png';
 
 const ServiceDetails = () => {
     const { id } = useParams();
     return (
         <div>
-            <h3>This is service area: {id}</h3>
+            <div className="row my-5">
+                <div className="col-md-5">
+                    <img src={detail} alt="" />
+                </div>
+                <div className="rounded col-md-7 my-5 mr-5" style={{ backgroundColor: "aquamarine" }}>
+                    <h2>Welcome to Service Details Area</h2>
+                    <h3>This is service area: {id}</h3>
+                </div>
+            </div>
+
         </div>
     );
 };
